6b3d162d41f7b375c41c683e195ce17680e63d31
Eduarda Lima <eududalim@gmail.com>
2020-03-12 11:26:25 -0300

    Commit - VS Code - Melhora na exibição dos dados

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\duda-\Git\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\BACKUP DE ARQUIVOS 17-09-19\Documents\dev\calculadora_de_imc"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\duda-\Git\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
